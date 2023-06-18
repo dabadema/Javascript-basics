26e50b1c9b3ec371423f42822a38a3448e012dc5
dabadema <dabadema7@hotmail.com>
2023-06-18 18:35:05 +0200

    Conditional ternary CMP & CMT

--- course-material-v2/script_assignments.js
+++ course-material-v2/script_assignments.js
@@ -260,13 +260,13 @@ switch(language){
     default:
         console.log('Great language too :D');
 }
-*/
+
 // THE CONDITIONAL (TERNARY) OPERATOR
 
 countryPopulation = prompt ("What is the population in millions of your country?");
 
 countryPopulation >= 33 ? console.log("Portugal's population is above average") : console.log("Portugal's population is below average");
-
+*/
     
 
 
