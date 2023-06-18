18ecd0a1d8b0feb0e283269b0a53d781956195eb
dabadema <dabadema7@hotmail.com>
2023-06-18 19:24:17 +0200

    Code challenge 4 completed

--- course-material-v2/script.js
+++ course-material-v2/script.js
@@ -314,4 +314,18 @@ if(age >=18) {
 console.log(drink2);
 
 console.log( `I like to drink ${age >= 18 ? `wine` : `water`}`);
-*/
+
+CODE CHALLENGE 4 
+
+(My solution)
+bill = prompt(`Introduce the bill's value`);
+let tip ;
+let total ;
+50 >= bill <= 300 ? console.log(`The bill was ${bill}, the tip was ${ tip = bill*0.15 } and the total value ${ parseInt(bill)+parseFloat(tip) }`) : console.log(`The bill was ${bill}, The tip was ${ tip = bill*0.20 } and the total value ${ parseInt(bill)+parseFloat(tip) }`) ;
+ 
+ (Cleaner solution)
+const bill = 275;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+ */
