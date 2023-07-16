54a1e661e841bbcccc0ae6586c87cd50745ab310
dabadema <dabadema7@hotmail.com>
2023-07-16 13:50:27 +0200

    Challengue 2 completed

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -3,6 +3,7 @@
 /*
 ///////////////////////////////////////
 // Activating Strict Mode
+
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -15,6 +16,7 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 ///////////////////////////////////////
 // Functions
+
 function logger() {
   console.log('My name is Jonas');
 }
@@ -193,7 +195,7 @@ console.log(ages);
 */
 ///////////////////////////////////////
 // Basic Array Operations
-
+/*
 const friends = [`Michael`, `Steven`, `Peter`];
 const newLength = friends.push(`Jay`);
 
@@ -223,3 +225,52 @@ console.log(friends.includes(23));
 if (friends.includes(`Steven`)) {
   console.log(`You have a friend called Steven`);
 }
+*/
+///////////////////////////////////////
+// Coding Challenge #2
+/*
+Steven wants you to improve his tip calculator, using the same rules as before — tip 15% of the bill if the bill value is
+between 50 and 300, and if the value is different, the tip is 20%.
+
+Your tasks:
+
+1) Write a function calcTip that takes any bill value as an input and returns the corresponding tip, calculated based on the
+rules above (you can check out the code from the first tip calculator challenge if you need to). 
+Use the function type you like the most. Test the function using a bill value of 100.
+
+2)And now let's use arrays! So, create an array called bills containing the test data below.
+
+3)Create an array called tips containing the tip value for each bill, calculated from the function you created before.
+
+4)BONUS: Create an array totals containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555, and 44.
+*/
+const calcTip = function(billValue){
+  if (50 < billValue && billValue< 300){
+    return billValue*0.15;
+  } else {
+    return billValue*0.20;
+  }
+}
+
+const functionTest = calcTip(100);
+console.log(functionTest);
+
+const bills = [125, 555, 44];
+console.log(bills);
+
+const tipBill1 = calcTip(bills[0]);
+const tipBill2 = calcTip(bills[1]);
+const tipBill3 = calcTip(bills[2]);
+
+const tips = [tipBill1, tipBill2, tipBill3];
+console.log(tips);
+
+/* Solution from the professor is simpler and having less variables
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]);
+console.log(tips); */
+
+const totals = [bills[0]+tips[0], bills[1]+tips[1], bills[2]+tips[2]];
+console.log(totals);
