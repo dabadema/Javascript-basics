61b29f5f82fc94020b85eaeafd61f67a101f4d87
dabadema <dabadema7@hotmail.com>
2023-08-23 23:19:40 +0200

    Looping backwards theory

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -431,6 +431,7 @@ for(let rep=1; rep <= 10; rep++){
   console.log(`Lifting weights repetition ${rep}`);
 }
 */
+///////////////////////////////////////////
 // LOOPING ARRAYS, BREAKING AND CONTINUING
 /*
 const jonas = [
@@ -478,6 +479,34 @@ for(let i = 0; i < jonas.length; i++){
   if(typeof jonas[i] === `number`) break;
 
   console.log(jonas[i], typeof jonas[i]);
+}
+*/
+
+///////////////////////////////////////
+// LOOPING BACKWARDS AND LOOPS IN LOOPS
+/*
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true
+];
+
+// 0, 1, ..., 4
+// 4, 3, ..., 0
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`-------- Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
+  }
 }
 */
 
