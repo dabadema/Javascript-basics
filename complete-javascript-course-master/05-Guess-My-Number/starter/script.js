7cf485ad171adfa3b7d3f2580e02e856f23c82d1
dabadema <dabadema7@hotmail.com>
2023-09-10 16:57:08 +0530

    Handling click events

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = `🎉 Correct Number!!`;
 console.log(document.querySelector('.message').textContent);
@@ -10,3 +11,14 @@ document.querySelector('.score').textContent = 10;
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    // This is the case when there is no input, 0 is always false, therefore "!" makes the case where there is no guess introduced, then the condition is fulfilled
+    document.querySelector('.message').textContent = `⛔️ No number!`;
+  }
+});
