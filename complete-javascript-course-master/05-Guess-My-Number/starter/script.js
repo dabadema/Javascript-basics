cfa60d00bdf2601c1abaa7debb39dabb209f7503
dabadema <dabadema7@hotmail.com>
2023-09-11 23:55:58 +0530

    Implementing the highscore

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -14,8 +14,8 @@ console.log(document.querySelector('.guess').value);
 */
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -53,8 +53,12 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.number').style.width = '30rem';
-  }
 
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  }
   // else if (guess < 0 || guess >= 21) {  FIXME (Let's see if someone can help me with this!)
   //   document.querySelector('.message').textContent = `⛔️ Please, introduce a number between 1 and 20`;
   // }
