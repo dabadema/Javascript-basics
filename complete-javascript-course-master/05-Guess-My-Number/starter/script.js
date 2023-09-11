d39d83b0139e4275be80dd90cf1b4f50732783bb
dabadema <dabadema7@hotmail.com>
2023-09-11 23:26:37 +0530

    Coding challenge 1 - Guess my name!

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -13,7 +13,7 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
@@ -21,7 +21,7 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
-  // When there is no input0 is always false, therefore "!" makes the case where there is no guess introduced, then the condition is fulfilled
+  // When there is no input, 0 is always false, therefore "!" makes the case where there is no guess introduced, then the condition is fulfilled
   if (!guess) {
     document.querySelector('.message').textContent = `⛔️ No number!`;
 
@@ -54,7 +54,34 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.number').style.width = '30rem';
   }
+
   // else if (guess < 0 || guess >= 21) {  FIXME (Let's see if someone can help me with this!)
   //   document.querySelector('.message').textContent = `⛔️ Please, introduce a number between 1 and 20`;
   // }
+});
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.message').textContent = `Start guessing...`;
+  document.querySelector('.number').textContent = `?`;
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
