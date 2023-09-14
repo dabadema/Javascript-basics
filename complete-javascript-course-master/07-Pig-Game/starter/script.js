b5d40f802d8bf717ed19a32f1633c3c740f0ce36
dabadema <dabadema7@hotmail.com>
2023-09-14 14:08:35 +0530

    Project #3 - Roll the dice

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Selecting elements
+let totalScoreP1El = document.querySelector('#score--0');
+let currentScoreP1El = document.getElementById('current--0');
+let totalScoreP2El = document.querySelector('#score--1');
+let currentScoreP2El = document.getElementById('current--1');
+let diceEl = document.querySelector('.dice');
+
+// Starting conditions
+totalScoreP1.textContent = 0;
+totalScoreP2.textContent = 0;
+diceEl.classList.add('hidden');
