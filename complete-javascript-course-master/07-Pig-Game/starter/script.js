e1597013712103ec2669648f226944d970ef6de7
dabadema <dabadema7@hotmail.com>
2023-09-17 19:45:43 +0200

    Switching the active player

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -5,6 +5,8 @@ const totalScoreP0El = document.querySelector('#score--0');
 const currentScoreP0El = document.getElementById('current--0');
 const totalScoreP1El = document.querySelector('#score--1');
 const currentScoreP1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 const diceEl = document.querySelector('.dice');
 const btnNewGameEl = document.querySelector('.btn--new');
@@ -16,7 +18,9 @@ totalScoreP0El.textContent = 0;
 totalScoreP1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 const rollsResult = function () {
@@ -30,11 +34,15 @@ const rollsResult = function () {
   if (diceRolls !== 1) {
     // Add dice to current score
     currentScore += diceRolls;
-    currentScoreP0El.textContent = currentScore; // FIXME change later to consider each player
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    // Switch to the next player
+    // Switch to the next player,
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
     currentScore = 0;
-    currentScoreP0El.textContent = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 };
 
